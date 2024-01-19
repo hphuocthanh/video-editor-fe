7e99560a98aafdcea8b048ae1078cc87b262a986
tomthanh <phuocthanhqt113@gmail.com>
2024-01-19 16:31:22 +1100

    update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/76449953/node-canvas-on-vercel-serverless-function-serverless-function-exceeds-the-maxi
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
